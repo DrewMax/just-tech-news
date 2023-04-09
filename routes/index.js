f2b09c3f106c3a90383913c7034834ff8b008e24
DrewMax <andrew.reviresco@gmail.com>
2023-04-09 11:43:36 +0200

    post routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
